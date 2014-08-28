9a2afe0ab8654032795c251b7ee264273437ceab
Brian Darby <bsdarby@gmail.com>
2014-08-27 22:16:33 -0700

    Works with database, blank panels and exit

---
+++ src/main/java/view/ScreenOutput.java
@@ -0,0 +1,29 @@
+package view;
+
+import model.VoterData;
+
+import javax.swing.*;
+
+/**
+ * Created by bsdarby on 8/27/14.
+ */
+@SuppressWarnings("DefaultFileTemplate")
+public class ScreenOutput extends JPanel{
+
+	JLabel vdScreenOutput;
+	JPanel mainPanel, voterPanel, historyPanel;
+	JButton done;
+	VoterData app;
+
+
+	public ScreenOutput (VoterData container) {
+		app = container;
+		mainPanel			= new JPanel();
+		voterPanel 		= new JPanel();
+		historyPanel	= new JPanel();
+		setSize(getMaximumSize());
+
+
+	}
+
+}
